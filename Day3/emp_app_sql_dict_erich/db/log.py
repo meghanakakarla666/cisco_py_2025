6443df0361d9363410f9d01ddf772d457860cad6
meghanakakarla <meghanakakarla666@gmail.com>
2025-09-15 09:40:05 +0530

    updated

--- Day3/emp_app_sql_dict_erich/db/log.py
+++ Day3/emp_app_sql_dict_erich/db/log.py
@@ -1,6 +1,6 @@
 import logging
 #setup 
 logging.basicConfig(
-                    filename='../..employee_app_logs.log',
+                    filename='../../employee_app_logs.log',
                     level=logging.INFO,
                     format='%(asctime)s - [%(levelname)s] - %(message)s')
