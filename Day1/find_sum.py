6baf6c75afb52c004f455575954c163946aedfd6
meghanakakarla <meghanakakarla666@gmail.com>
2025-09-15 09:07:06 +0530

    updated

---
+++ Day1/find_sum.py
@@ -0,0 +1,21 @@
+# Simple program to find the sum of two numbers
+
+# Input two numbers from the user
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+
+# # Calculate the sum
+# sum_result = num1 + num2
+
+# # Display the result
+# print("The sum is:", sum_result)
+
+def find_sum(num1, num2):
+    return num1 + num2
+
+# Calculate the sum using the function
+res1 = find_sum(10.5, 20.5)
+print("The sum is:", res1)
+
+res2 = find_sum(5, 6)
+print("The sum is:", res2)
