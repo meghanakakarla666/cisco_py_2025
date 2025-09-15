0ecfe1f5ffa2677cf5559378227d1b34b39c8a27
meghanakakarla <meghanakakarla666@gmail.com>
2025-09-15 09:24:30 +0530

    updated

--- Day3/flight_app_sql_dict_erich/db/log.py
+++ Day3/flight_app_sql_dict_erich/db/log.py
@@ -2,7 +2,8 @@ import logging
 
 # setup
 logging.basicConfig(
-    filename='flight_app_logs.log',
+    # filename='flight_app_logs.log',
+    filename='../../flight_app_logs.log',
     level=logging.INFO,
     format='%(asctime)s - [%(levelname)s] - %(message)s'
 )
